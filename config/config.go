cf4a404d321a53894d02d75d8156944aa6db73b5
Anthony Wang <anthonyywang05@gmail.com>
2025-07-04 17:46:52 -0700

    feat(setup): Add flag parsing

---
+++ config/config.go
@@ -0,0 +1,65 @@
+package config
+
+import (
+	"log"
+	"strings"
+)
+
+type TargetStage int
+const (
+	TargetStageAll TargetStage = iota
+	TargetStageScan
+	TargetStageParse
+	TargetStageInter
+	TargetStageAssembly
+)
+
+var stageNameToTargetStage = map[string]TargetStage{
+	"": TargetStageAll,
+	"scan": TargetStageScan,
+	"parse": TargetStageParse,
+	"inter": TargetStageInter,
+	"assembly": TargetStageAssembly,
+}
+
+type Config struct {
+	InputPath string
+	Stage TargetStage
+	OutputPath string
+	Optimizations []string
+	Debug bool
+}
+
+func Load(
+	inputPath string,
+	stagePtr *string,
+	outputPathPtr *string,
+	optPtr *string,
+	debugPtr *bool,
+) *Config {
+	config := &Config{
+		InputPath: inputPath,
+	}
+
+	if stagePtr != nil {
+		stage, ok := stageNameToTargetStage[*stagePtr]
+		if !ok {
+			log.Fatalf("invalid target stage %s", *stagePtr)
+		}
+		config.Stage = stage
+    }
+
+	if outputPathPtr != nil {
+		config.OutputPath = *outputPathPtr
+	}
+
+	if optPtr != nil {
+		config.Optimizations = strings.Split(*optPtr, ",")
+    }
+
+	if debugPtr != nil {
+        config.Debug = *debugPtr
+    }
+
+	return config
+}
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "fmt"
+	"log"
+    "os"
+
+	"github.com/spf13/pflag"
+
+	"github.com/cosmicgenius/decaf-go/config"
+)
+
+func main() {
+    var (
+		stage = pflag.String("target", "", "Target stage (scan, parse, inter, assemble, or empty to run all)")
+		output = pflag.String("output", "", "Output file")
+		opt = pflag.String("opt", "", "Optimizations")
+		debug = pflag.Bool("debug", false, "Debug mode")
+	)
+    
+    // Parse flags
+    pflag.Parse()
+    
+    // Check for exactly one non-flag argument
+    if pflag.NArg() != 1 {
+        fmt.Fprintf(os.Stderr, "Usage: %s <filename> [flags]\n", os.Args[0])
+        os.Exit(1)
+    }
+    
+    // Get the filename argument
+    filename := pflag.Arg(0)
+    
+    fmt.Printf("Processing file: %s\n", filename)
+
+	config := config.Load(filename, stage, output, opt, debug)
+
+	log.Printf("Config: %+v", config)
+    
+	// TODO
+}
