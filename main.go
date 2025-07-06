d637a374fe858e86b5727cdd79179505b5893d89
Anthony Wang <anthonyywang05@gmail.com>
2025-07-05 22:59:37 -0700

    feat(setup): Add output file writing

--- main.go
+++ main.go
@@ -8,12 +8,13 @@ import (
 	"github.com/spf13/pflag"
 
 	"github.com/cosmicgenius/decaf-go/config"
+	"github.com/cosmicgenius/decaf-go/output"
 )
 
 func main() {
     var (
 		stage = pflag.String("target", "", "Target stage (scan, parse, inter, assemble, or empty to run all)")
-		output = pflag.String("output", "", "Output file")
+		outputPath = pflag.String("output", "", "Output file. If not set, output to stdout.")
 		opt = pflag.String("opt", "", "Optimizations")
 		debug = pflag.Bool("debug", false, "Debug mode")
 	)
@@ -30,11 +31,19 @@ func main() {
     // Get the filename argument
     filename := pflag.Arg(0)
 
-    fmt.Printf("Processing file: %s\n", filename)
+	config := config.Load(filename, stage, outputPath, opt, debug)
 
-	config := config.Load(filename, stage, output, opt, debug)
+	if config.OutputPath != "" {
+		outputFile, err := os.Create(config.OutputPath)
+		if err != nil {
+			log.Fatalf("Failed to open output file: %s", err)
+		}
+		defer outputFile.Close()
 
-	log.Printf("Config: %+v", config)
+		output.SetWriter(outputFile)
+    }
+
+	output.Writef("Config: %+v\n", config)
 
 	// TODO
 }
---
+++ output/output.go
@@ -0,0 +1,25 @@
+package output
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+var writer io.Writer = os.Stderr
+
+func SetWriter(w io.Writer) {
+	writer = w
+}
+
+func Writef(format string, v ...any) {
+    fmt.Fprintf(writer, format, v...)
+}
+
+func Write(v ...any) {
+	fmt.Fprint(writer, v...)
+}
+
+func Writeln(v ...any) {
+	fmt.Fprintln(writer, v...)
+}
